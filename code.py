38e9e12ebe06920bbaf0aceb6743a83b60eb5f5d
Allergicboe <91279649+Allergicboe@users.noreply.github.com>
2025-02-21 19:03:06 -0300

    Update code.py

--- code.py
+++ code.py
@@ -62,7 +62,7 @@ def find_rows(selected_cuenta, selected_sectores, data):
 
 # Actualizar celdas
 def update_steps(rows, steps_updates, consultoria_value, comentarios_value):
-    now = datetime.now().strftime('%d-%m-%Y %H:%M')
+    now = datetime.now().strftime('%d-%m-%y %H:%M')
     cells_to_update = []
 
     # Actualizar Consultoría
